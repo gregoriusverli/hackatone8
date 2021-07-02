394d319072be9e6a5a1b8f3887b2db52494957ca
gregorius-tech <gregoriusverly@gmail.com>
2021-07-02 12:18:16 +0700

    final

--- loginPage.js
+++ loginPage.js
@@ -22,7 +22,7 @@ const dataLogin = [
     {
         username: 'azrullahkainage',
         password: 'aji123'
-    },
+    }
 ]
 
 
@@ -61,7 +61,7 @@ loginButton.addEventListener ('click', (e) => {
         console.log(output)
         
     if (output === true) {
-        return window.location.replace("/hackatone8/homepage.html")
+        return window.location.replace("/homepage.html")
     } else {
         return incorrectPassword.innerHTML = `Incorrect username or password`
     }
